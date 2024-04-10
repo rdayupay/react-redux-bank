20d7ef2137125943d2ab1c0b29b869fb3607276f
rdayupay <raqueldayupay@gmail.com>
2024-04-10 21:50:05 +0800

    Implement API calls with thunk

--- src/features/accounts/AccountOperations.jsx
+++ src/features/accounts/AccountOperations.jsx
@@ -16,13 +16,12 @@ function AccountOperations() {
     balance,
   } = useSelector((store) => store.account);
 
-  console.log(balance);
-
   function handleDeposit() {
     if (!depositAmount) return;
 
-    dispatch(deposit(depositAmount));
+    dispatch(deposit(depositAmount, currency));
     setDepositAmount('');
+    setCurrency('');
   }
 
   function handleWithdrawal() {
