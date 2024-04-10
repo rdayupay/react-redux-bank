f71d12999e29a555973e8f863117d3f9f82d4d06
rdayupay <raqueldayupay@gmail.com>
2024-04-10 21:12:45 +0800

    Dispatch actions from app

--- src/features/customers/Customer.jsx
+++ src/features/customers/Customer.jsx
@@ -1,8 +1,7 @@
 import { useSelector } from 'react-redux';
+
 function Customer() {
   const customer = useSelector((store) => store.customer.fullName);
-
-  console.log(customer);
 
   return <h2>👋 Welcome, {customer}</h2>;
 }
