aac9d72925d2a8d2db0e3658dfa6f661ebfaa016
rdayupay <raqueldayupay@gmail.com>
2024-04-10 19:25:42 +0800

    Add action creators

--- src/store.jsx
+++ src/store.jsx
@@ -37,3 +37,22 @@ function reducer(state = initialState, action) {
 }
 
 const store = createStore(reducer);
+
+function deposit(amount) {
+  return { type: 'account/deposit', payload: amount };
+}
+
+function withdraw(amount) {
+  return { type: 'account/withdraw', payload: amount };
+}
+
+function requestLoan(amount, purpose) {
+  return {
+    type: 'account/requestLoan',
+    payload: { amount, purpose },
+  };
+}
+
+function payLoan() {
+  return { type: 'account/payLoan' };
+}
